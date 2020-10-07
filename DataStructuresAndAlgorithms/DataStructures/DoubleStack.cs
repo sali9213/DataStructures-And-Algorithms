308c39c4d3421e995407fc099d60d7b5123191c4
Syed Ali <ali.syed.w2@gmail.com>
2020-10-07 21:28:12 +0100

    Implement 2 stacks using a single array

---
+++ DataStructuresAndAlgorithms/DataStructures/DoubleStack.cs
@@ -0,0 +1,163 @@
+﻿using System;
+namespace DataStructuresAndAlgorithms.DataStructures
+{
+    public class DoubleStack
+    {
+        private int[] stack;
+        private int topLeft;
+        private int topRight;
+        private int divider;
+
+        public DoubleStack(int n)
+        {
+            stack = new int[n];
+            // The first element in the left stack
+            divider = (int)Math.Floor((double)n / 2);
+            topLeft = divider;
+            topRight = divider + 1;
+
+        }
+
+        public void pushLeft(int elem)
+        {
+            if (this.Full())
+            {
+                throw new Exception("Overflow exception");
+            }
+
+            if(topLeft == -1)
+            {
+                ShiftRight();   
+            }
+
+            stack[topLeft] = elem;
+            topLeft--;
+        }
+
+        public void pushRight(int elem)
+        {
+            if (this.Full())
+            {
+                throw new Exception("Overflow exception");
+            }
+           
+            if (topRight == stack.Length)
+            {
+                ShiftLeft();
+                stack[topRight] = elem;
+            }
+
+            stack[topRight] = elem;
+            topRight++;
+          
+        }
+
+        public int popLeft()
+        {
+            if (EmptyLeft())
+            {
+                throw new Exception("Underflow exception");
+            }
+            else
+            {
+                topLeft++;
+                return stack[topLeft];
+            }
+        }
+
+        public int popRight()
+        {
+            if (EmptyRight())
+            {
+                throw new Exception("Underflow exception");
+            }
+            else
+            {
+                topRight--;
+                return stack[topRight];
+            }
+        }
+
+        public void peakLeft()
+        {
+
+        }
+
+        public void peekRight()
+        {
+
+        }
+
+        public bool Full()
+        {
+            if (topLeft == -1 && topRight == stack.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool EmptyLeft()
+        {
+            if (topLeft == divider)
+                return true;
+            return false;
+        }
+
+        public bool EmptyRight()
+        {
+            if (topRight == divider + 1)
+                return true;
+            return false;
+        }
+
+        private void ShiftRight()
+        {
+            for(int i = topRight; i  > 0; i--)
+            {
+                stack[i] = stack[i - 1];
+            }
+
+            topLeft++;
+            topRight++;
+            divider++;
+            
+        }
+
+        private void ShiftLeft()
+        {
+            for(int i = topLeft; i < stack.Length - 1; i++)
+            {
+                stack[i] = stack[i + 1];
+            }
+
+            topLeft--;
+            topRight--;
+            divider--;
+        }
+
+        public void Display()
+        {
+            for(int i = 0; i < stack.Length; i++)
+            {
+                if(i == topLeft || i == topRight)
+                {
+                    Console.Write("||" + stack[i] + "|| ");
+                }
+                else if (i == divider)
+                {
+                    Console.Write("|" + stack[i] + "| ");
+                }
+                else
+                {
+                    Console.Write(stack[i] + " ");
+                }
+            }
+        }
+
+
+
+
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -7,7 +7,21 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-            
+            DoubleStack stack = new DoubleStack(7);
+            stack.pushLeft(3);
+            stack.pushRight(4);
+            stack.pushLeft(2);
+            stack.pushLeft(1);
+            stack.pushLeft(5);
+            stack.pushLeft(7);
+            stack.pushLeft(10);
+            stack.popLeft();
+            stack.pushRight(14);
+            stack.popLeft();
+            stack.pushRight(16);
+            stack.popLeft();
+            stack.pushRight(11);
+            stack.Display();
         }
     }
 }
